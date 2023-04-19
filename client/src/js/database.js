a9beea81fe64ca9c3e40898239b995b0397c8053
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-19 10:44:39 -0400

    working on local storage

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
 import { openDB } from 'idb';
 
+const dataBase= 'jate'
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +15,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>  {
+  const contactDb = await openDB(dataBase, 1);
+  const tx = contactDb.transaction(dataBase, 'readwrite');
+  const store = tx.objectStore(dataBase)
+  const request = store.add({id: 1, jate: content})
+  
+  const result = await request;
+  console.log('saved to the database', result)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB(dataBase, 1)
+  const tx = contactDb.transaction(dataBase, 'readonly')
+  const store = tx.objectStore(dataBase)
+  const request = store.getAll();
+
+  const result = await request;
+  console.log(result);
+  return result
+
+};
 
 initdb();
