650d12d2a134fdce189c100e45867f7630ddb350
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-19 14:10:20 -0400

    can type, it saves to the IndexedDB, and pulls from IndexedDE, but it does not print to screen

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,41 +1,70 @@
 import { openDB } from 'idb';
 
-const dataBase= 'jate'
+const dataBase = 'jate';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) =>  {
-  const contactDb = await openDB(dataBase, 1);
-  const tx = contactDb.transaction(dataBase, 'readwrite');
-  const store = tx.objectStore(dataBase)
-  const request = store.add({id: 1, jate: content})
-  
-  const result = await request;
-  console.log('saved to the database', result)
+const upgradeDb = (db) => {
+  if (db.objectStoreNames.contains(dataBase)) {
+    console.log(`${dataBase} database already exists`);
+    return;
+  }
+  db.createObjectStore(dataBase, { keyPath: 'id', autoIncrement: true });
+  console.log(`${dataBase} database created`);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+
+const initdb = async () => {
+  try {
+    const db = await openDB('jate', 1, {
+      upgrade(db) {
+        if (db.objectStoreNames.contains('jate')) {
+          console.log('jate database already exists');
+          return;
+        }
+        db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        console.log('jate database created');
+      },
+    });
+    console.log('Database opened successfully');
+    return db;
+  } catch (error) {
+    console.error('Failed to open database:', error);
+    throw error;
+  }
+};
+
+// Add content to the database
+export const putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction(dataBase, 'readwrite');
+    const store = tx.objectStore(dataBase);
+    const request = store.put({ id: 1, jate: content });
+    const result = await request;
+    console.log('saved to the database', result);
+    return result;
+  } catch (error) {
+    console.error('Failed to add content to the database:', error);
+    throw error;
+  }
+};
+
+// Get all content from the database
 export const getDb = async () => {
-  const contactDb = await openDB(dataBase, 1)
-  const tx = contactDb.transaction(dataBase, 'readonly')
-  const store = tx.objectStore(dataBase)
-  const request = store.getAll();
-
-  const result = await request;
-  console.log(result);
-  return result
-
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error('Failed to get content from the database:', error);
+    throw error;
+  }
 };
 
 initdb();
+
+
